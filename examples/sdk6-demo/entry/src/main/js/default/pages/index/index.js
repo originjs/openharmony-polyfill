7302cca33f8cf9ef51fb45d6db6c859d6c22a2ed
joey <flyfish.zy@gmail.com>
2021-12-18 15:15:28 +0800

    add http example for sdk6

--- examples/sdk6-demo/entry/src/main/js/default/pages/index/index.js
+++ examples/sdk6-demo/entry/src/main/js/default/pages/index/index.js
@@ -1,8 +1,30 @@
+import 'openharmony-polyfill'
+import axios from 'axios'
+
 export default {
     data: {
-        title: ""
+        result: ""
     },
-    onInit() {
-        this.title = this.$t('strings.world');
+    onClickAxios() {
+        this.result = 'request...'
+        axios.get('https://developer.harmonyos.com/cn/develop/')
+            .then((res) => {
+                console.log('---------')
+                console.log(res.data)
+                this.result = res.statusText
+            }).catch(error => {
+            this.result = error
+        });
+    },
+
+    onClickFetch() {
+        this.response = 'request...'
+        console.log('---------')
+        fetch('https://developer.harmonyos.com/cn/develop/')
+            .then(res => {
+                console.log('-----------')
+                console.log(res.data)
+                this.response = res.statusText;
+            });
     }
 }
