8c5d462b101b3d21860ab70b49859ad35c290d0f
joey <flyfish.zy@gmail.com>
2021-12-18 15:01:02 +0800

    init of sdk6 demo

---
+++ examples/sdk6-demo/entry/src/main/java/org/originjs/openharmony/polyfilldemo/MainAbility.java
@@ -0,0 +1,16 @@
+package org.originjs.openharmony.polyfilldemo;
+
+import ohos.ace.ability.AceAbility;
+import ohos.aafwk.content.Intent;
+
+public class MainAbility extends AceAbility {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+}
