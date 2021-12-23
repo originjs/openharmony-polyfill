73e9214dac56389b1d6cf4fcee246243c241996b
ruleeeer <me@ruleeeer.cn>
2021-12-23 19:15:55 +0800

    feat: add index.d.ts and remove ts-ignore

--- index.d.ts
+++ index.d.ts
@@ -9,16 +9,66 @@ declare class XMLHttpRequest {
     readonly statusText: string;
     timeout: number;
     readonly upload: XMLHttpRequestUpload;
+
     abort(): void;
+
     getAllResponseHeaders(): string;
+
     getResponseHeader(name: string): string | null;
+
     open(method: string, url: string | URL, async: boolean, username?: string | null, password?: string | null): void;
+
     overrideMimeType(mime: string): void;
+
     send(body?: Document | null): void;
+
     setRequestHeader(name: string, value: string): void;
+
     readonly DONE: number;
     readonly HEADERS_RECEIVED: number;
     readonly LOADING: number;
     readonly OPENED: number;
     readonly UNSENT: number;
+}
+
+declare class TextEncoder {
+    constructor(encoding?: string);
+
+    readonly encoding: string;
+
+    encode(str: string): Uint8Array;
+
+    encodeInto(str: string, arr: Uint8Array): { read: number, written: number }
+}
+
+declare class TextDecoder {
+    constructor(encoding?: string, options?: { fatal?: boolean, ignoreBOM?: boolean }) ;
+
+    readonly encoding: string;
+    readonly fatal: boolean;
+    readonly ignoreBOM: boolean
+
+    decode(buffer?: ArrayBuffer, options?: { stream: boolean }): string
+}
+
+declare class URL {
+    constructor(url: string, base?: string) ;
+
+    hash: string;
+    host: string;
+    hostname: string;
+    href: string;
+
+    toString(): string;
+
+    readonly origin: string;
+    password: string;
+    pathname: string;
+    port: string;
+    protocol: string;
+    search: string;
+    readonly searchParams: URLSearchParams;
+    username: string;
+
+    toJSON(): string;
 }
