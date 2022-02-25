806b53f2ae2e0717d0feb5bc6538e07894875635
joey <flyfish.zy@gmail.com>
2022-02-25 15:16:24 +0800

    feat: add webpack-plugin-demo

---
+++ examples/webpack-plugin-demo/README.md
@@ -0,0 +1,4 @@
+## openharmony-webpack-plugin demo
+### **!!!Very important**
+You need to install `@originjs/openharmony-webpack-plugin` before you start.
+see [packages/webpack-plugin/README.md](../../packages/webpack-plugin/README.md) for details.
---
+++ examples/webpack-plugin-demo/entry/src/main/java/org/originjs/openharmony/webpackplugindemo/MyApplication.java
@@ -0,0 +1,10 @@
+package org.originjs.openharmony.webpackplugindemo;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
